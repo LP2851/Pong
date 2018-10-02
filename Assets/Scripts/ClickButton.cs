97be3af01e18943201d652011538258dec73fedf
LP2851 <lucas.phillips28@hotmail.com>
2018-10-02 11:53:08 +0100

    Added base code
    Basic code done

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickButton : MonoBehaviour {
+
+    public void GotoScene1()
+    {
+        SceneManager.LoadScene("Scene 1");
+    }
+
+    public void GotoScene2()
+    {
+        SceneManager.LoadScene("Scene 2");
+    }
+
+    public void GoToScene3()
+    {
+        SceneManager.LoadScene("Scene 3");
+    }
+
+    public void GotoScene4()
+    {
+        SceneManager.LoadScene("Scene 4");
+    }
+
+    public void GoToScene5()
+    {
+        SceneManager.LoadScene("Scene 5");
+    }
+
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GotoVersions()
+    {
+        SceneManager.LoadScene("Versions");
+    }
+
+}
+   
